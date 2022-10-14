d61fecdec95fdc23ab507baa2e62d564abca340f
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-10-14 23:10:41 +0600

    Angular 14 DotNET 6 App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employees-list',
+  templateUrl: './employees-list.component.html',
+  styleUrls: ['./employees-list.component.css']
+})
+export class EmployeesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
