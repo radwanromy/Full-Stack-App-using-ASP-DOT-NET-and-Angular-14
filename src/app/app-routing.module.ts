e201bf69ae24a90208bfb8feef13c43e691d63dd
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-10-17 23:18:48 +0600

    App routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EmployeesListComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
